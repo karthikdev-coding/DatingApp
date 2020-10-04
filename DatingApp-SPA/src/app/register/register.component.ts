005d37c9c899b7ecc057649d77bf0dd4d9d7b282
karthik <karthik.parcha@gmail.com>
2020-10-04 13:06:20 +0530

    Client login and regiser

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { runInThisContext } from 'vm';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+model: any = {};
+@Output() cancelRegister = new EventEmitter();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registraion successful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
