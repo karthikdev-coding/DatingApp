783d097b5d9f803aa837bfcd70133255f72ed61e
karthik <karthik.parcha@gmail.com>
2020-10-17 05:45:53 +0530

    Extending Api controller

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+            .ForMember(destinationMember => destinationMember.PhotoUrl, opt => 
+              opt.MapFrom(sourceMember => sourceMember.Photos.FirstOrDefault(p => p.IsMain).Url))
+              .ForMember(destinationMember => destinationMember.Age, opt => 
+              opt.MapFrom(sourceMember => sourceMember.DateOfbirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+            .ForMember(destinationMember => destinationMember.PhotoUrl, opt => 
+              opt.MapFrom(sourceMember => sourceMember.Photos.FirstOrDefault(p => p.IsMain).Url))
+              .ForMember(destinationMember => destinationMember.Age, opt => 
+              opt.MapFrom(sourceMember => sourceMember.DateOfbirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
